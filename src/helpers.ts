aa0bb5089bb3f5f61c145394c53c6e2a22e2ac17
Amine Aydoun <mohamed.amine.aydoun@bondify.com>
2021-05-28 12:02:04 +0200

    feat: add contain method and remove getSize

--- src/helpers.ts
+++ src/helpers.ts
@@ -8,8 +8,8 @@ export const removeDuplicates = (arr: numberOrString[]): numberOrString[] => {
   return [];
 };
 
-export const getSize = (collection: any): number => {
-  return 0;
+export const contains = (object: any, value: numberOrString): boolean => {
+  return false;
 };
 
 export const sumToTarget = (arr: number[], target: number) => {
